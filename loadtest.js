fe2a948955e5007384c99260dc05816fd5294e26
Nikolas Burk <nikolas.burk@gmail.com>
2022-10-14 20:01:55 +0200

    update prisma

--- loadtest.js
+++ loadtest.js
@@ -14,6 +14,64 @@ export let options = {
   duration: '10s',
 }
 
+/**
+   500: http_req_failed................: 30.97% ✓ 902       ✗ 2010   
+   14.51% 22.52%
+
+       ✗ status 200 (get posts)
+        ↳  16% — ✓ 83 / ✗ 418
+       ✗ status 200 (create post)
+        ↳  31% — ✓ 155 / ✗ 336
+       ✗ status 200 (create comment)
+        ↳  75% — ✓ 116 / ✗ 38
+       ✗ status 200 (view post)
+        ↳  63% — ✓ 96 / ✗ 56
+       ✗ status 200 (like post)
+        ↳  60% — ✓ 84 / ✗ 54
+
+  250: http_req_failed................: 11.72% ✓ 513        ✗ 3863 
+
+       ✗ status 200 (get posts)
+        ↳  66% — ✓ 337 / ✗ 172
+       ✗ status 200 (create post)
+        ↳  76% — ✓ 390 / ✗ 119
+       ✗ status 200 (create comment)
+        ↳  85% — ✓ 334 / ✗ 56
+       ✗ status 200 (view post)
+        ↳  81% — ✓ 318 / ✗ 72
+       ✗ status 200 (like post)
+        ↳  75% — ✓ 296 / ✗ 94
+
+  ???
+  100: http_req_failed................: 43.61% ✓ 560       ✗ 724  
+
+       ✗ status 200 (get posts)
+        ↳  6% — ✓ 17 / ✗ 259
+       ✗ status 200 (create post)
+        ↳  10% — ✓ 30 / ✗ 246
+       ✗ status 200 (create comment)
+        ↳  70% — ✓ 21 / ✗ 9
+       ✗ status 200 (view post)
+        ↳  33% — ✓ 10 / ✗ 20
+       ✗ status 200 (like post)
+        ↳  13% — ✓ 4 / ✗ 26
+
+  50: http_req_failed................: 31.30% ✓ 273       ✗ 599 
+
+         ✗ status 200 (get posts)
+        ↳  20% — ✓ 29 / ✗ 114
+       ✗ status 200 (create post)
+        ↳  34% — ✓ 50 / ✗ 93
+       ✗ status 200 (create comment)
+        ↳  84% — ✓ 42 / ✗ 8
+       ✗ status 200 (view post)
+        ↳  50% — ✓ 25 / ✗ 25
+       ✗ status 200 (like post)
+        ↳  34% — ✓ 17 / ✗ 33
+
+  5: 
+ */
+
 const SLEEP_DURATION = 0.1
 
 // const baseUrl = 'http://localhost:3000/api'
@@ -26,6 +84,7 @@ const baseUrl = `https://${__ENV.API_URL}/api`
 // View post          http PUT localhost:3000/api/post/1/views
 // Like post          http PUT localhost:3000/api/post/1/likes
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function () {
   group('user flow', function () {
     // Get posts
@@ -81,3 +140,21 @@ export default function () {
     LikePostTrend.add(createLikeRes.timings.duration)
   })
 }
+
+
+/**
+ * vus = 1000
+ * 
+ * 
+ * █ user flow
+    ✗ status 200 (get posts)
+    ↳  87% — ✓ 365 / ✗ 53
+    ✗ status 200 (create post)
+    ↳  97% — ✓ 319 / ✗ 7
+    ✗ status 200 (create comment)
+    ↳  98% — ✓ 286 / ✗ 3
+    ✗ status 200 (view post)
+    ↳  99% — ✓ 217 / ✗ 2
+    ✗ status 200 (like post)
+    ↳  97% — ✓ 192 / ✗ 4 
+ */
