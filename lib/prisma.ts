4d3b448c53610a2bc03a578bc0b3b790733e916f
Nikolas Burk <nikolas.burk@gmail.com>
2022-10-15 12:41:58 +0200

    minor updates

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,7 +11,7 @@ declare global {
 }
 
 const prisma = global.prisma || new PrismaClient({
-  log: ['error']
+  log: ['error', 'info', 'query']
 })
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
